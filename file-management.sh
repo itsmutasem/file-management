dbb05ba6ea9193affb04e8b21ff388c31ebcfe70
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-06-04 23:24:23 +0300

    Update pwd Command

--- file-management.sh
+++ file-management.sh
@@ -1,4 +1,4 @@
-# where we are?, Print working Directory
+# where we are?, Print working Directory, it outputs the absolute path of the current directory you are in.
 pwd
 # The ls command is used to list files and directories in the current working directory
 ls
