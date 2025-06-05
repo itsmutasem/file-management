8b2d4f65ff9a2c44492a00ce10bb3c60bffc2cbc
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-06-06 01:35:41 +0300

    RE - ls Command

--- file-management.sh
+++ file-management.sh
@@ -1,3 +1,8 @@
 #  🔍 Show the absolute path of the current working directory
 pwd
 
+#  📁 List files and directories
+ls             # Simple list
+ls -l          # Long listing with permissions, size, and date
+ls -a          # Include hidden files (like .git, .env)
+
